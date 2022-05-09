1873f8c4713f55fd78c8a60bda3755aada1dddcb
9909hjh <9909hjh@gmail.com>
2022-05-09 15:09:48 +0900

    test
    test

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char* argv[])
     SDL_Color color_Blue = { 0, 0, 255 };
     SDL_Color color_White = { 255, 255, 255 };
 #ifdef UNICODE
-    SDL_Surface* temp_surf = TTF_RenderUNICODE_Shaded(m_pFont, (Uint16*)L"한글 텍스쳐", color_Blue, color_White);
+    SDL_Surface* temp_surf = TTF_RenderUNICODE_Shaded(m_pFont, (Uint16*)L"한글 텍스쳐@테스트", color_Blue, color_White);
 #else
     SDL_Surface* temp_surf = TTF_RenderUTF8_Shaded(m_pFont, "한글 텍스쳐", color_Blue, color_White);
 #endif
